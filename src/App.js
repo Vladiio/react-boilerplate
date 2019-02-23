dc017172ee653932bb0b1af2f4e56c5fdf1a456d
Vlad <vladpochta5@gmail.com>
2019-02-23 15:35:57 +0200

    Setup bundle analyzer, externalyze react dependencies

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
-
-console.log(module);
+import { hot } from 'react-hot-loader/root';
 
 class App extends React.Component {
   state = { count: 0 };
 
   render() {
     return (
-      <div>
+      <div
+        style={{
+          background:
+            this.state.count > 5 ? 'red' : 'black'
+        }}
+      >
         <h1 className="header">Counter</h1>
         <button
           onClick={() =>
@@ -34,4 +38,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default hot(App);
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,19 @@
 const merge = require('webpack-merge');
 const baseConf = require('./webpack.config.base');
-
+const {
+  BundleAnalyzerPlugin
+} = require('webpack-bundle-analyzer');
 module.exports = merge(baseConf, {
-  mode: 'production'
+  mode: 'production',
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+      reportFilename: 'bundle_analytics.html'
+    })
+  ],
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  }
 });
